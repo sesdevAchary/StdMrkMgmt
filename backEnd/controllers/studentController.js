82a34a8196f3a61766cfea0006b0b7aa04781bd9
sesdevAchary <sesdev.achary@gmail.com>
2024-09-30 08:38:55 +0000

    unfinished code

--- backEnd/controllers/studentController.js
+++ backEnd/controllers/studentController.js
@@ -23,3 +23,55 @@ exports.createStudentInfo=async(req,res)=>{
 
 
 // get single student information
+
+exports.getstudentInfo=async(res,req)=>{
+    try{
+        const studentInfo=await  studentModel.find(); //get all the student info from the db
+        res.send(studentInfo);
+    }catch(err){
+        res.status(400).send(err.message)
+    }
+    
+}
+
+
+
+// get student by Id 
+
+exports.getstudentInfoById= async(req,res)=>{
+    try{
+        const studentInfoById=await studentModel.findById(req.params.id);
+        if(!studentInfoById)return res.status(404).send("Student id not found");
+        res.send(studentInfoById);
+    }catch(err){
+        res.status(400).send(err.message)
+    }
+}
+
+
+// update student information by Id
+exports.updateStudentInfo=async(req,res)=>{
+    try{
+        const updateInfo = await studentModel.updateById(req.params.id, {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            unique_id: req.body.unique_id,
+            mail_id: req.body.mail_id,
+            current_address: req.body.current_address,
+            attendence: req.body.attendence,
+            total_score: req.body.total_score,
+            avg_cgpa: req.body.avg_cgpa
+
+
+        },{new:true}); // return the updated code .......
+        if(!updateInfo) return res.status(404).send("Updated room not found")
+            else{
+        res.send("Updated room found ",updateInfo);
+        };
+    } 
+   
+    catch(err){
+        res.status(400).send(error)
+    }
+
+    }
