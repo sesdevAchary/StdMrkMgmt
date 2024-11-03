bcf82b15e893881b850fc78496d33f94d6a67394
sesdevAchary <sesdev.achary@gmail.com>
2024-11-03 05:41:32 +0000

    student mark management

--- client/src/component/studentList.js
+++ client/src/component/studentList.js
@@ -2,3 +2,15 @@
  import {Link} from   'react-router-dom'; /* to navigate between the routes */
  import axios from    'axios'; /* promise based http for client making request*/
  import Notification from './Notfification';    /* for displaying notification to the user*/
+
+
+
+ // To retrive the url from the environment//
+ const API_URL= process.env.REACT_APP_API_URL;
+
+
+ // functional component for state initialisation//
+  const studentList =() =>{
+         const[student,setStudent]= useState([]);
+         const[notification ,setNotification]=useState( ' ');
+  }
