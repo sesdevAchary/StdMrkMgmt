36bb0d42cde653c21bb469e252156a01eea21aa4
sesdevAchary <sesdev.achary@gmail.com>
2024-11-04 13:28:46 +0000

    student mark management

--- client/src/component/studentCreate.js
+++ client/src/component/studentCreate.js
@@ -3,4 +3,4 @@ import axios from 'axios';
 import {useNavigate} from 'react-router-dom';
 import Notification from './notification'
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = process.env.REACT_APP_API_URL; /* Retrieves the API URL from the environment variables, which is used to send requests to the server.*/
