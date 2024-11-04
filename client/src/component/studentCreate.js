59b10d5b04018e80c067de0d896cdcf38e1368b9
sesdevAchary <sesdev.achary@gmail.com>
2024-11-04 13:28:21 +0000

    student mark management

--- client/src/component/studentCreate.js
+++ client/src/component/studentCreate.js
@@ -2,3 +2,5 @@ import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 import {useNavigate} from 'react-router-dom';
 import Notification from './notification'
+
+const API_URL = process.env.REACT_APP_API_URL;
