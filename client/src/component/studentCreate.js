ea87db043af4344a70ea585fec5d58072ff928e3
sesdevAchary <sesdev.achary@gmail.com>
2024-11-04 13:36:27 +0000

    student mark management

--- client/src/component/studentCreate.js
+++ client/src/component/studentCreate.js
@@ -4,3 +4,10 @@ import {useNavigate} from 'react-router-dom';
 import Notification from './notification'
 
 const API_URL = process.env.REACT_APP_API_URL; /* Retrieves the API URL from the environment variables, which is used to send requests to the server.*/
+
+
+const PatientAdd = ({onPatientAdd=()=>{}})=>{   {/* functional component with a prop*/}
+    const [name,setName]=useState(' ');
+    
+
+}
