01f40a4882e16773095993b5a328682b364c331f
sesdevAchary <sesdev.achary@gmail.com>
2024-11-06 18:16:53 +0000

    student mark management

--- client/src/component/studentCreate.js
+++ client/src/component/studentCreate.js
@@ -59,5 +59,7 @@ return(
                 
 
                 <select type='select' placeholder="SelectGender" value={gender} onChange={(e) => setGender(e.target.value)} required className='input-field'>
-
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Other">Other</option> </select>
 )
