03b788ab68a789f24ada05adf9ef65e5acb78a9e
sesdevAchary <sesdev.achary@gmail.com>
2024-11-08 18:18:05 +0000

    student mark management

--- client/src/component/Notification.js
+++ client/src/component/Notification.js
@@ -2,5 +2,5 @@ import React from 'react'
 
 // to extract values from the props object to be passed into the fun.component //
 const Notification = ({message,onClose})=>{   {/*message contains data about notification like text(the message content ) and type (notification type like success,error) */}
-
+ const notificationClass = `notification ${message.type}`;
 }
