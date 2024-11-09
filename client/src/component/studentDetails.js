a76b694e4b70a54f3af3799c61bc8f02f724b14c
sesdevAchary <sesdev.achary@gmail.com>
2024-11-09 12:54:15 +0000

    studnet mark management

---
+++ client/src/component/studentDetails.js
@@ -0,0 +1,3 @@
+import React,{useState,useEffect}from 'react'; /*React hooks for managing state and side effects in functional components.*/
+import axios from 'axios';    /* a promise based http client for making request */
+import {useParams,Link,useNavigate} from 'react-router-dom';
