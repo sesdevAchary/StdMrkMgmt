f3b36cb4c58405cbee278f0d551005ed95c18407
sesdevAchary <sesdev.achary@gmail.com>
2024-11-09 13:04:04 +0000

    studnet mark management

--- client/src/component/studentDetails.js
+++ client/src/component/studentDetails.js
@@ -1,3 +1,11 @@
 import React,{useState,useEffect}from 'react'; /*React hooks for managing state and side effects in functional components.*/
 import axios from 'axios';    /* a promise based http client for making request */
-import {useParams,Link,useNavigate} from 'react-router-dom';
+import {useParams,Link,useNavigate} from 'react-router-dom';  /* useParams: To access the route parameters (specifically, the student ID in this case).*/
+import Notification from './Notification';
+
+
+// API URL ENVIRONMENT VARIABLES //
+
+const API_URL = process.env.REACT_APP_API_URL;
+console.log(API_URL);   /* for debugging the apI url*/
+
