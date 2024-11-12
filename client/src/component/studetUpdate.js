f0116d2d62a09c0c3620e480c43c32414fbff1b6
sesdevAchary <sesdev.achary@gmail.com>
2024-11-12 18:04:00 +0000

    studnet mark management

--- client/src/component/studetUpdate.js
+++ client/src/component/studetUpdate.js
@@ -3,3 +3,4 @@ import {useParams,useNavigate} from 'react-router-dom';
 import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL;
+console.log(API_URL) // logs the URL to the console for debugging purposes.//
