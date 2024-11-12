8198535b2b50de850d7d8185d3a9fb7a19a81288
sesdevAchary <sesdev.achary@gmail.com>
2024-11-12 18:02:44 +0000

    studnet mark management

--- client/src/component/studetUpdate.js
+++ client/src/component/studetUpdate.js
@@ -1,3 +1,5 @@
 import React,{useEffect,useState} from 'react';
 import {useParams,useNavigate} from 'react-router-dom';
 import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL;
