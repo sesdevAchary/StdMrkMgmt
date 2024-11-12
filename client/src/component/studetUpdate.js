8344a8e921c3aa7d27190cc467bad6eef050cae9
sesdevAchary <sesdev.achary@gmail.com>
2024-11-12 18:11:08 +0000

    studnet mark management

--- client/src/component/studetUpdate.js
+++ client/src/component/studetUpdate.js
@@ -4,3 +4,9 @@ import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL;
 console.log(API_URL) // logs the URL to the console for debugging purposes.//
+
+
+const studentId=()=>{
+    const {id} =useParams();
+    
+}
