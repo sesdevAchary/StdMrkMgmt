36bdad709bfa96a4a67ee3a0950d429eff110193
sesdevAchary <sesdev.achary@gmail.com>
2024-11-26 17:39:55 +0000

    student mark management

--- client/src/component/studentDelete.js
+++ client/src/component/studentDelete.js
@@ -2,3 +2,4 @@ import React from 'react'
 
 const DeleteStudent = ({ id, onDelete }) => {
     const handleDelete = () => {
+        if (window.confirm('Are you sure to delete this Student information?')){
