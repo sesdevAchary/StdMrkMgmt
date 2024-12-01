3cd54712b2a337b2984465350018f525f5bbede3
sesdevAchary <sesdev.achary@gmail.com>
2024-12-01 06:14:00 +0000

    student mark management

--- client/src/component/homePage.js
+++ client/src/component/homePage.js
@@ -2,3 +2,10 @@ import react from 'react';
 import {Link } from 'react-router-dom';
 import { Container, Typography, Button, Box } from '@mui/material';
 
+
+const  HomePage = () => {
+    return (
+      <Container maxWidth="lg" sx={{ textAlign: 'center', py: 5 }}>
+        <Typography variant="h2" component="h1" color="success">
+          Welcome to the Clinic Management System
+        </Typography>
