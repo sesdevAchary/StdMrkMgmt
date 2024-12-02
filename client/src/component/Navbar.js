88a53eca1f7c09c535bcaec5bdf343c3470fcc35
sesdevAchary <sesdev.achary@gmail.com>
2024-12-02 06:29:09 +0000

    student mark management

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -3,3 +3,13 @@ import { Link as RouterLink } from 'react-router-dom';
 import {
   AppBar,
   Toolbar,
+  Typography,
+  Button,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+} from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import HomeIcon from '@mui/icons-material/Home';
+import MenuBookIcon from '@mui/icons-material/MenuBook'; // Added Notes icon
