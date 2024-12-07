11b55e72dcc4e77dfc1941c95f47baf3f9332f0b
sesdevAchary <sesdev.achary@gmail.com>
2024-12-07 06:28:27 +0000

    student mark management

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+import Footer from './components/footer';
+import Navbar from './components/Navbar';
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+    <Box display="flex" flexDirection="column" minHeight="100vh">
+        <div className="box-container">
+            <Navbar />
+            <Routes>
+                {/* <Route path="/list" element={<PatientList />} />
+                <Route path="/add" element={<PatientAdd />} />
+                <Route path="/edit/:id" element={<PatientEdit />} />
+                <Route path="/detail/:id" element={<PatientDetail />} />
+                <Route exact path='/' element={<HomePage />} /> */}
+            </Routes>
+        </div>
+        <Footer />
+        </Box>
+    </Router>
+
+    
+);
+};
 
 export default App;
