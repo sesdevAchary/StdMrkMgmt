d49509ec77de0cb0d8ea8ecbb4fb12939d87270c
sesdevAchary <sesdev.achary@gmail.com>
2024-12-08 06:59:09 +0000

    student mark managemet

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import footer from './component/footer';
 import Navbar from './component/Navbar';
 import HomePage from './component/homePage';
 import './App.css'; // Make sure this contains your CSS
+import { blueGrey } from '@mui/material/colors';
 
 
 
@@ -25,20 +26,22 @@ const App = () => {
       <CssBaseline />
     <Router>
     <Box display="flex" flexDirection="column" minHeight="100vh">
+
         <div className="box-container">
-            <Navbar />
+            
             <Routes>
-                 {<Route path="/list" element={<studentList />} />
+                 <Route path="/list" element={<studentList />} />
                 <Route path="/add" element={<PatientAdd />} />
                 <Route path="/edit/:id" element={<PatientEdit />} />
-                <Route path="/detail/:id" element={<PatientDetail />} />
+                <Route path="/detail/:id" element={<studentDetail />} />
                 <Route exact path='/' element={<HomePage />} /> 
             </Routes>
+
         </div>
         <footer/>
         </Box>
     </Router>
-    router fro all case 
+    
     </ThemeProvider>
     
 );
