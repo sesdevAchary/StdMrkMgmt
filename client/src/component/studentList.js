8bc6d2a57cb7a720b9afa2b5dbe599ea75976feb
sesdevAchary <sesdev.achary@gmail.com>
2024-12-09 04:52:33 +0000

    student mark managemet

--- client/src/component/studentList.js
+++ client/src/component/studentList.js
@@ -1,7 +1,7 @@
  import React,{useState,useEffect} from 'react';
  import {Link} from   'react-router-dom'; /* to navigate between the routes */
  import axios from    'axios'; /* promise based http for client making request*/
- import Notification from './Notfification';    /* for displaying notification to the user*/
+ import Notification from './Notification';    /* for displaying notification to the user*/
 
 
 
