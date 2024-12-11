5e12e9b3b1c8ba54642a1b1fb83d5592cd13cb01
sesdevAchary <sesdev.achary@gmail.com>
2024-12-11 12:37:53 +0000

    student mark managemet

--- client/src/component/StudentCreate.js
+++ client/src/component/StudentCreate.js
@@ -1,104 +1,7 @@
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// import { useNavigate } from 'react-router-dom';
-// import Notification from './Notification';
-
-// const API_URL = process.env.REACT_APP_API_URL; // Retrieves the API URL from environment variables
-
-// const StudentAdd = ({ onPatientAdd = () => {} ) => {
-//   const [name, setName] = useState(''); // Initialize with empty strings
-//   const [gender, setGender] = useState('');
-//   const [roll_no, setNumber] = useState('');
-//   const [showNotification, setShowNotification] = useState(null);
-
-//   const navigate = useNavigate(); // Correct use of useNavigate
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-
-//     if (!name || !gender || !roll_no) return; // Don't proceed if fields are empty
-
-//     try {
-//       const response = await axios.post(API_URL, { name, gender, roll_no });
-//       const newStudentID = response.data.id;
-
-//       // Clear form fields
-//       setName('');
-//       setgender('');
-//       setNumber('');
-
-//       // Show success notification
-//       setShowNotification({ type: 'success', text: `Patient "${response.data.name}" added successfully!` });
-
-//       // Navigate to the new student's detail page after 2 seconds
-//       setTimeout(() => navigate(`/detail/${newStudentID}`), 2000);
-//     } catch (error) {
-//       console.error('Error while adding a new student', error);
-//       setShowNotification({ type: 'error', text: 'Failed to add new student. Please try again.' });
-//     }
-//   };
-
-//   const handleCloseNotification = () => {
-//     setShowNotification(null);
-//   };
-
-//   return (
-//     <div className="box-container">
-//       <h2>Add Patient</h2>
-//       <form onSubmit={handleSubmit} className="form-container">
-
-//         <input
-//           type="text"
-//           placeholder="Name"
-//           value={name}
-//           onChange={(e) => setName(e.target.value)}
-//           required
-//           className="input-field"
-//         />
-//          <br/>
-
-//         <select
-//           placeholder="Select Gender"
-//           value={gender}
-//           onChange={(e) => setGender(e.target.value)}
-//           required
-//           className="input-field"
-//         >
-//                     <option value="select gender">Select geder</option>
-
-//           <option value="Male">Male</option>
-//           <option value="Female">Female</option>
-//           <option value="Other">Other</option>
-//         </select>
-//         <br/>
-//         <input
-//           type="number"
-//           placeholder="Roll No"
-//           value={roll_no}
-//           onChange={(e) => setNumber(e.target.value)}
-//           required
-//           className="input-field"
-//         />
-//         <br/>
-//         <div className="button-group">
-//           <button type="submit" className="btn btn-add">
-//             Add Student
-//           </button>
-//           <button type="button" className="btn btn-cancel" onClick={() => navigate('/')}>
-//             Cancel
-//           </button>
-//         </div>
-//       </form>
-//       {showNotification && <Notification message={showNotification} onClose={handleCloseNotification} />}
-//     </div>
-//   );
-// };
-
-// export default StudentAdd;
 
 
 import React, { useState } from 'react';
-import Link from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { slide, ToastContainer, toast } from 'react-tostify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -154,7 +57,7 @@ const CreateStudent = (props) => {
           theme: "light",
           transition: slide,
         });
-        setTimeOut = (() => {
+        setTimeout = (() => {
           setToast(false); // Hide the toast
           navigate('/'); // Navigate to homepage
         }, 5000); // Adjust the timeout as needed
@@ -207,7 +110,7 @@ const CreateStudent = (props) => {
               <p className='lead text-center'>Create A New Student</p>
 
 
-              <form noValidate onSubmit={onsubmit}>
+              <form noValidate onSubmit={onSubmit}>
 
                 <div className="all-form">
                   <input
@@ -215,7 +118,7 @@ const CreateStudent = (props) => {
                     placeholder='Enter the Name of the STUDENT'
                     name='first_name'
                     className='form-controller'
-                    value='student.first_name'
+                    value={student.first_name}
                     onChange={onChange}
                   />
 
@@ -227,8 +130,8 @@ const CreateStudent = (props) => {
                     placeholder='Enter the Roll number of the STUDENT'
                     name='unique_id'
                     className='form-controller'
-                    value='student.unique_id'
-                    onChange={onchange}
+                    value={student.unique_id}
+                    onChange={onChange}
                   />
                 </div>
 
@@ -239,8 +142,8 @@ const CreateStudent = (props) => {
                     placeholder='Enter the mail ID of the STUDENT'
                     name='mail_id'
                     className='form-controller'
-                    value='student.mail_id'
-                    onChange={onchange}
+                    value={student.mail_id}
+                    onChange={onChange}
                   />
                 </div>
 
@@ -252,8 +155,8 @@ const CreateStudent = (props) => {
                     placeholder='Enter the current address  of the STUDENT'
                     name='current_address'
                     className='form-controller'
-                    value='student.current_address'
-                    onChange={onchange}
+                    value={student.current_address}
+                    onChange={onChange}
                   />
                 </div>
 
@@ -263,8 +166,8 @@ const CreateStudent = (props) => {
                     placeholder='Enter the attendence number  of the STUDENT'
                     name='attendence'
                     className='form-controller'
-                    value='student.attendence'
-                    onChange={onchange}
+                    value={student.attendence}
+                    onChange={onChange}
                   />
                 </div>
 
@@ -274,8 +177,8 @@ const CreateStudent = (props) => {
                     placeholder='Enter the total score   of the STUDENT'
                     name=' total_score'
                     className='form-controller'
-                    value='student.total_score'
-                    onChange={onchange}
+                    value={student.total_score}
+                    onChange={onChange}
                   />
                 </div>
 
@@ -285,8 +188,8 @@ const CreateStudent = (props) => {
                     placeholder='Enter the cgpa obtained by the STUDENT'
                     name=' avg_cgpa'
                     className='form-controller'
-                    value='student.avg_cgpa'
-                    onChange={onchange}
+                    value={student.avg_cgpa}
+                    onChange={onChange}
                   />
                 </div>
 
