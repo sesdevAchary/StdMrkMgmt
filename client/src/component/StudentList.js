aa9212a0ee1cb505e045e55ba3bd896d5d5d0651
sesdevAchary <sesdev.achary@gmail.com>
2024-12-13 18:11:01 +0000

    student mark management

--- client/src/component/StudentList.js
+++ client/src/component/StudentList.js
@@ -61,3 +61,9 @@ import React ,{useState, useEffect} from "react";
 import Link from 'react-router-dom';
 import axios from 'axios';
 import { Button, Typography, Container, Grid, CircularProgress, Box } from '@mui/material';
+
+
+function StudentList()={
+    const [student,setStudent]=useState('');
+    
+}
