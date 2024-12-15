c2a25604645a31d53ca23498f84f7b297fe91195
sesdevAchary <sesdev.achary@gmail.com>
2024-12-15 17:43:15 +0000

    student mark management

--- client/src/component/StudentCard.js
+++ client/src/component/StudentCard.js
@@ -11,7 +11,20 @@ const StudentCard= ({Students})=>{
             display: 'flex',
             flexDirection: 'column',
             transition: 'transform 0.2s, box-shadow 0.2s',
+            borderRadius: 5,
+            boxShadow: 7,
+            '&:hover': {
+                transform: 'scale(1.05)',
+                boxShadow: 6,
         }}
+        >
+
+
+        <img
+        src='https://eacademics.in/img/i5.png'
+        alt='Students'
+        style={{ height: 200, objectFit: 'cover', width: '100%' }}
+    />
     )
 }
 
