932d033a118f82f7c7b3639709ea153cbf38c504
sesdevAchary <sesdev.achary@gmail.com>
2024-12-15 17:50:33 +0000

    student mark management

--- client/src/component/StudentCard.js
+++ client/src/component/StudentCard.js
@@ -24,15 +24,31 @@ const StudentCard= ({Students})=>{
         src='https://eacademics.in/img/i5.png'
         alt='Students'
         style={{ height: 200, objectFit: 'cover', width: '100%' }}
-    />
+         />
 
 
     <cardContent sx={{flexGrow:1}}>
         <Typography variant='h6' component='div' color='primary' gutterBottom>
-            
+        <Link to={`/show-student/${student._id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                        {student.name}
+                    </Link>
         </Typography>
     </cardContent>
-    )
-}
+    
+
+    <Typography variant="body2" color="text.secondary" sx={{ mt: 1 }}
+    style={{
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        display: '-webkit-box',
+        WebkitLineClamp: 3,
+        WebkitBoxOrient: 'vertical',
+    }}>
+    {student.total_score}
+</Typography>
+
+   </Card>
+    );
+};
 
 export default StudentCard;
