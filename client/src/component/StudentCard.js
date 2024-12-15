4ce439d4878069328f205c9fbae7b7def0bdd633
sesdevAchary <sesdev.achary@gmail.com>
2024-12-15 18:00:42 +0000

    student mark management

--- client/src/component/StudentCard.js
+++ client/src/component/StudentCard.js
@@ -4,60 +4,58 @@ import { Link } from 'react-router-dom';
 
 
 const StudentCard= ({Students})=>{
-    return(
+    return (
         <Card
-      sx={{
-        height: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        transition: 'transform 0.2s, box-shadow 0.2s',
-        borderRadius: 2,
-        boxShadow: 3,
-        '&:hover': {
-          transform: 'scale(1.05)',
-          boxShadow: 6,
-        },
-      }}
-    >
-      <img
-        src='https://images.unsplash.com/photo-1495446815901-a7297e633e8d'
-        alt='Books'
-        style={{ height: 200, objectFit: 'cover', width: '100%' }}
-      />
-      <CardContent sx={{ flexGrow: 1 }}>
-        <Typography variant="h6" component="div" color="primary" gutterBottom>
-          <Link to={`/show-book/${book._id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-            {book.title}
-          </Link>
-        </Typography>
-        <Typography variant="subtitle1" color="text.secondary">
-          {book.author}
-        </Typography>
-        <Typography variant="body2" color="text.secondary" sx={{ mt: 1 }}
-                    style={{
-                      overflow: 'hidden',
-                      textOverflow: 'ellipsis',
-                      display: '-webkit-box',
-                      WebkitLineClamp: 3,
-                      WebkitBoxOrient: 'vertical',
-                    }}>
-          {book.description}
-        </Typography>
-      </CardContent>
-      <Box sx={{ p: 2, mt: 'auto' }}>
-        <Button
-          component={Link}
-          to={`/show-book/${book._id}`}
-          variant="contained"
-          color="primary"
-          size="small"
-          fullWidth
+            sx={{
+                height: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                transition: 'transform 0.2s, box-shadow 0.2s',
+                borderRadius: 2,
+                boxShadow: 3,
+                '&:hover': {
+                    transform: 'scale(1.05)',
+                    boxShadow: 6,
+                },
+            }}
         >
-          View Details
-        </Button>
-      </Box>
-    </Card>
-  );
+            <img
+                src='https://eacademics.in/img/i5.png'
+                alt='Students'
+                style={{ height: 300, objectFit: 'cover', width: '100%' }}
+            />
+            <CardContent sx={{ flexGrow: 1 }}>
+                <Typography variant='h6' component='div' color='primary' gutterBottom>
+                    <Link to={`/show-student/${student._id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                        {student.first_name}
+                    </Link>
+                </Typography>
+              
+                <Typography variant="body2" color="text.secondary" sx={{ mt: 1 }}
+                    style={{
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                        display: '-webkit-box',
+                        WebkitLineClamp: 3,
+                        WebkitBoxOrient: 'vertical',
+                    }}>
+                    {student.total_score}
+                </Typography>
+            </CardContent>
+            <Box sx = {{p: 2, mt: 'auto'}}>
+                <Button
+                  component={Link}
+                  to={`/show-student/${student.unique_id}`}
+                  variant='contained'
+                  color='primary'
+                  size='small'
+                  fullWidth
+                >
+                    view Details
+                </Button>
+            </Box>
+        </Card>
+    );
 };
 
 export default StudentCard;
