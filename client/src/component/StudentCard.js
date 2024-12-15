4b0c423e47fdb110160200de4776b4eadd47352d
sesdevAchary <sesdev.achary@gmail.com>
2024-12-15 17:26:23 +0000

    student mark management

--- client/src/component/StudentCard.js
+++ client/src/component/StudentCard.js
@@ -1,3 +1,8 @@
 import React from 'react';
 import { Card, CardContent, Typography, Button, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
+
+
+const StudentCard= ({Students})=>{
+    
+}
