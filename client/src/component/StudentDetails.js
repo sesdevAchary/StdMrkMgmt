f82f69345b2f28231107a0d7249729cb4b4f1c76
sesdevAchary <sesdev.achary@gmail.com>
2024-12-18 16:59:27 +0000

    student Mark Management

--- client/src/component/StudentDetails.js
+++ client/src/component/StudentDetails.js
@@ -1,3 +1,13 @@
 
 import React ,{useState , useEffect} from "react";
 import { useparams, Link as RouterLink ,useNavigate } from 'react-router-dom';
+import {
+    Container,
+    Paper,
+    Typography,
+    Grid,
+    Button,
+    Card,
+    CardMedia,
+    Divider,
+    Box, } from '@mui/material';
