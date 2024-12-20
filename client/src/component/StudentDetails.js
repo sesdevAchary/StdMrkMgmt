fef9a29292392f18f29a5c68505f3e42ed63a026
sesdevAchary <sesdev.achary@gmail.com>
2024-12-20 18:58:51 +0000

    student mark management

--- client/src/component/StudentDetails.js
+++ client/src/component/StudentDetails.js
@@ -224,3 +224,9 @@ import {
   DialogContentText,
   DialogTitle,
 } from "@mui/material";
+
+import { styled } from "@mui/material/styles";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import axios from "axios";
