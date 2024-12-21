c322ed14739d58aad79d38f6d52291e1339e9770
sesdevAchary <sesdev.achary@gmail.com>
2024-12-21 12:32:37 +0000

    student mark management

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
+import {ThemeModeProvider } from './components/ThemeMode';
 import { CssBaseline, Box } from '@mui/material';
 import ExportPage from './component/exportPages';
 
