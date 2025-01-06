cf8555506bb624669b7a7825d7e2d078cd42cf26
sesdevAchary <sesdev.achary@gmail.com>
2025-01-06 03:25:59 +0000

    student mark management

--- client/src/component/StudentCard.js
+++ client/src/component/StudentCard.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { Card, CardContent, Typography, Button, Box } from '@mui/material';
+import { Card, CardContent, Typography, Button, Box, Avatar } from '@mui/material';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types'
-
+import PropTypes from 'prop-types';
 
 const StudentCard = ({ student }) => {
     return (
         <Card
             sx={{
-                height: '100%',
                 display: 'flex',
                 flexDirection: 'column',
+                height: '100%',
                 transition: 'transform 0.2s, box-shadow 0.2s',
                 borderRadius: 4,
                 boxShadow: 3,
@@ -20,55 +19,63 @@ const StudentCard = ({ student }) => {
                 },
             }}
         >
-            <img
-                src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fvector%2Faccount-login-profile-gm1126662487-296701928&psig=AOvVaw2SWnQZmlK1k1MnwuY39j4z&ust=1736185686951000&source=images&cd=vfe&opi=89978449&ved=2ahUKEwifzKzqkd-KAxWyS2wGHU18GkEQjRx6BAgAEBg'
-                alt='Students'
-                style={{ height: 200, objectFit: 'cover', width: '100%' }}
+            <Avatar
+                alt={student.first_name}
+                src={student.imageUrl || 'https://img.myloview.com/stickers/online-financial-advisor-icon-simple-illustration-of-online-financial-advisor-vector-icon-for-web-design-isolated-on-white-background-700-208789481.jpg'}
+                sx={{
+                    width: '100%',
+                    height: 200,
+                    objectFit: 'cover',
+                }}
             />
             <CardContent sx={{ flexGrow: 1 }}>
-                <Typography variant='h6' component='div' color='primary' gutterBottom>
-                    <Link to={`/show-student/${student._id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                <Typography variant="h6" component="div" color="primary" gutterBottom>
+                    <Link
+                        to={`/show-student/${student._id}`}
+                        style={{ textDecoration: 'none', color: 'inherit' }}
+                    >
                         {student.first_name}
                     </Link>
                 </Typography>
 
-                {/* <Typography variant="body2" color="text.secondary" sx={{ mt: 1 }}
-                    style={{
-                        overflow: 'hidden',
-                        textOverflow: 'ellipsis',
-                        display: '-webkit-box',
-                        WebkitLineClamp: 3,
-                        WebkitBoxOrient: 'vertical',
-                    }}>
-                    {student.total_score}
-                </Typography> */}
+                {student.total_score !== undefined && (
+                    <Typography variant="body2" color="text.secondary" sx={{ mt: 1 }}>
+                        Total Score: {student.total_score}
+                    </Typography>
+                )}
             </CardContent>
             <Box sx={{ p: 2, mt: 'auto' }}>
                 <Button
                     component={Link}
                     to={`/details/${student._id}`}
-                    variant='h4'
-                    color='primary'
-                    size='small'
+                    variant="contained"
+                    color="primary"
+                    size="small"
                     fullWidth
                 >
-                    View this Stuedent Detail
+                    View Student Details
                 </Button>
             </Box>
         </Card>
     );
 };
 
-
-// to ensure that the student prop is passed correctly //
-
+// Ensuring props are correctly validated
 StudentCard.propTypes = {
     student: PropTypes.shape({
         _id: PropTypes.string.isRequired,
         first_name: PropTypes.string.isRequired,
-        total_score: PropTypes.number.isRequired,
-        imageUrl: PropTypes.string, // Optional if an image URL exists
+        total_score: PropTypes.number,
+        imageUrl: PropTypes.string,
     }).isRequired,
+};
+
+// Providing default values for optional props
+StudentCard.defaultProps = {
+    student: {
+        imageUrl: '', // Default empty image URL
+        total_score: 0, // Default score if not provided
+    },
 };
 
 export default StudentCard;
