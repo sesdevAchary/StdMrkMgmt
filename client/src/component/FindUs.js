a7967998ad21d18d4c9cc556ca3e9a53e36273e0
sesdevAchary <sesdev.achary@gmail.com>
2025-01-22 12:56:43 +0000

    fixing footer  issues

--- client/src/component/FindUs.js
+++ client/src/component/FindUs.js
@@ -17,9 +17,9 @@ const FindUs = () => (
         variant="h4"
         sx={{
           color: 'text.primary',
-          fontWeight: 600,
-          fontSize: '1.6rem',
-          letterSpacing: 1.1,
+          fontWeight: 700,
+          fontSize: '1.8rem',
+          letterSpacing: 1.0,
           mb: 3,
         }}
       >
@@ -37,15 +37,15 @@ const FindUs = () => (
       <Box
         sx={{
           mt: 3,
-          borderRadius: '8px',
+          borderRadius: '15px',
           overflow: 'hidden',
-          boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
+          boxShadow: '0 4px 12px rgba(243, 231, 231, 0.1)',
         }}
       >
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14956.04539827178!2d85.9088800637985!3d20.4236141549822!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a190cd8341668fd%3A0x136fb0e392733c82!2sBerhampur%2C%20Brahmapur%2C%20Odisha%20754112!5e0!3m2!1sen!2sin!4v1737347071371!5m2!1sen!2sin"
           width="100%"
-          height="300"
+          height="200"
           style={{ border: 0 }}
           allowFullScreen=""
           loading="lazy"
